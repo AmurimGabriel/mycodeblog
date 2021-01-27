9660a094f42c08d9a0a2fcd1d2695ca263c20f4a
AmurimGabriel <amurim.souza27@gmail.com>
2021-01-26 21:57:20 -0300

    Corrindo erros de digitacao

--- codeblog/src/main/java/com/spring/codeblog/configuration/SecurityConfig.java
+++ codeblog/src/main/java/com/spring/codeblog/configuration/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("souza").password("123").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("souza").password("{noop}123").roles("ADMIN");
     }
 
     @Override
